2548ec1050ab77d4f007ef62782f71caac313254
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-11-09 23:48:18 +0100

    javascribt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
-// let result1 = document.getElementById("result1").innerHTML
-// let result2 = document.getElementById("result2").innerHTML
-// let test = document.getElementById("test").innerHTML
-function durch() {
-    let result1 = document.getElementById("result1").innerHTML
-let result2 = document.getElementById("result2").innerHTML
-let test = document.getElementById("test").innerHTML
+let result1=document.getElementById("result1")
+let result2=document.getElementById("result2")
+
+
+function loschen() {
+    result1.innerHTML = " " 
+    result2.innerHTML = " " 
     
-    result1 = test
-    
+}
+function prozent() {
+    result1.innerHTML= "%"
+    prozentt = result1/100
+    result2.innerHTML=prozentt 
+}
+
+function zahl(myValue) {
+    result1.innerHTML += myValue
+
+}
+function gleisch() {
+    result1.innerHTML+= "="
+    let solved =eval(result1)
+    result2.innerHTML=solved
+}
