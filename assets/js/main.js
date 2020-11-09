f775b62e8d51799e4f3475260505d82a67de6940
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-11-09 21:52:35 +0100

    tachenrechner

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// let result1 = document.getElementById("result1").innerHTML
+// let result2 = document.getElementById("result2").innerHTML
+// let test = document.getElementById("test").innerHTML
+function durch() {
+    let result1 = document.getElementById("result1").innerHTML
+let result2 = document.getElementById("result2").innerHTML
+let test = document.getElementById("test").innerHTML
+    
+    result1 = test
+    
